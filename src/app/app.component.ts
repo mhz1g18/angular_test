569a0cc5a8b47077049eca04024e6e8fca7a2860
mzlatev@unionproject.eu <mzlatev@unionproject.eu>
2021-04-11 11:58:29 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { Observable, of } from 'rxjs';
+import { delay, map, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+
+interface Item {
+  id: Number,
+  name: String,
+  email: String,
+  username: String,
+  address: Object,
+  phone: String,
+  website: String,
+  company: Object,
+  
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.test.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test-app';
+
+  items$: Item[];
+  selectedItemName = ''
+
+  constructor(private http: HttpClient){
+
+  }
+
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>('https://jsonplaceholder.typicode.com/users');
+  }
+
+  ngOnInit() {
+     this.getItems().subscribe(res => {
+      this.items$ = res;
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgSelectModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
